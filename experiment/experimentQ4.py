5f54d6ed71fa3a412d4b1a4d55def6362bbd1bf6
Yan, Sixing <plutoyem@outlook.com>
2018-01-10 16:01:44 +0800

    new

--- experiment/experimentQ4.py
+++ experiment/experimentQ4.py
@@ -1,58 +1,24 @@
+# -*- coding: utf-8 -*-
 """
     Test Q4, the Eq( ) 
 """
-"""
-Experiment
-    foreach Epsilon [1,10]
-        foreach dataset (8)
-            foreach h {300,500,1000,2000}, w {10,15}
-                foreach skech {cM,gM}
-                    repeat 10 times 
-                        SKETCHstd: STD of sketch
-                        SKETCHmean: MEAN of sketch
-                        non-over: if estimated-average < Epsilong * SKETCHstd
-                        ratio of non-over/total
-                    get MEANratio & STDratio of these 10 ratios
-            }
-
-data format
-
-dataset={
-    'dataset':,
-    'h':,
-    'sketch':,
-    'SKETCHmean',
-    'SKETCHstd':.
-    }
-
-sketch={
-     'Epsilon':,
-     'dataset':,
-     'h':,
-     'sketch':,
-     'MEANratio':,
-     'STDratio':.
-    }                                   
-"""
-
 #===================  Import ->
 # system
 import sys; sys.path.append("..")
-from copy import deepcopy
-import random
+import copy 
 import numpy as np
 import os; os.chdir("D:/Alfonso Ngan/Documents/Github Project/Sketch-for-Data-Stream/experiment")
 # DIY
-import lib.diyTool
+import lib.diyTool as diyTool
 #===================  <- Import
 
 #===================  path area ->
 homePath = 'D:/Alfonso Ngan/Documents/Github Project/Sketch-for-Data-Stream/'# use '/' as ending
 
-Q0result_Sketch_CS_Path = homePath+'experiment/result/Q0_sketchResultData1'
-Q0result_Dataset_CS_Path = homePath+'experiment/result/Q0_datasetResultData1'
-Q0result_Sketch_GM_Path = homePath+'experiment/result/Q0_sketchResultData2'
-Q0result_Dataset_GM_Path = homePath+'experiment/result/Q0_datasetResultData2'
+Q4result_Rad_Path = homePath + 'Q4_Rad'
+Q4result_Top_Path = homePath + 'Q4_Top'
+Q4evaluate_Rad_Path = homePath + 'Q4_Rad_evaluate'
+Q4evaluate_Top_Path = homePath + 'Q4_Rad_evaluate'
 #===================  <- path area
 
 #================  parameter ->
@@ -60,48 +26,130 @@ wSet = [10,15]
 hSet = [300,500,1000]
 Epsilon = [i+1 for i in range(10)]
 dataset = [ 
-    #['D:/google desk PC/ip_graph_refined',4213084,2,'ip', 80],
-    ['C:/Users/alfonso.yan/Documents/graph_freq_comp18.txt',338239,2,'comp18', 0.90],
-    ['C:/Users/alfonso.yan/Documents/graph_freq_comp16.txt',1391333,2,'comp16', 0.90],
-    ['C:/Users/alfonso.yan/Documents/graph_freq_comp14.txt',7904564,2,'comp14', 0.70],
-    ['D:/google desk PC/ip_graph_refined',4213084,2,'ip', 0.90],
-    ['C:/Users/alfonso.yan/Documents/tweet_stream_hashed_refined',17813281,2,'tweet', 0.60],
-    #['C:/Users/alfonso.yan/Documents/graph_freq_comp12.txt',338239,2,'comp18', 90],
-    ['C:/Users/alfonso.yan/Documents/graph_freq_comp10.txt',1372146644,2,'comp1', 0.05]
+    ['C:/Users/alfonso.yan/Documents/graph_freq_comp18.txt',338239,'comp18', 0.90],
+    ['C:/Users/alfonso.yan/Documents/graph_freq_comp16.txt',1391333,'comp16', 0.90],
+    ['C:/Users/alfonso.yan/Documents/graph_freq_comp14.txt',7904564,'comp14', 0.70],
+    ['D:/google desk PC/ip_graph_refined',4213084,'ip', 0.90],
+    ['C:/Users/alfonso.yan/Documents/tweet_stream_hashed_refined',17813281,'tweet', 0.60],
+    ['C:/Users/alfonso.yan/Documents/graph_freq_comp10.txt',1372146644,'comp1', 0.05]
 ]
-
-repeatNumber = 10 # repeat times
+percent = [0.01,0.03,0.05,0.1,0.2]
+evaType = {'rad':['rad_mean','rad_medium','rad_max'],'top':['top_mean','top_medium','top_max']}
+h = 300
+w = 10
+increase = 30
 #================ <- parameter
+def getRecord(w,h,dsName,ty,dataset):
+    #
+    for ky in dataset:
+        if dataset[ky]['w']==w and dataset[ky]['h']==h and dataset[ky]['dsName']==dsName:
+            recordList = [dataset[ky][way] for way in evaType[ty]]
+            return recordList
+    print('not exist')
+    return None
 
+def Coverage(expectRange, trueRange, OElist):
+    #
+    h1Range = [(OElist[i]+1)*increase for i in range(len(OElist))]
+    low = expectRange[0]
+    up = expectRange[1]
+    low_true = trueRange[0]
+    up_true = trueRange[1]
 
+    upH1 = min([up,up_true])
+    lowH1 = max([low,low_true])
 
+    coverOE = [] 
+    trueOE = []
+    for i in range(len(OElist)):
+        if h1Range[i] < upH1 and h1Range[i] > lowH1:
+            coverOE.append(OElist[i])
+        if h1Range[i] < up_true and h1Range[i] > low_true:
+            trueOE.append(OElist[i]) 
 
+    coverPrecent = abs(upH1-lowH1)/(up_true-low_true)
+    coverageOE = sum(coverOE)/sum(trueOE)
+    return coverPrecent, coverageOE
 
-#================
+def RelativeBias(expectOpt, trueOpt, OElist):
+    #
+    h1Range = [(OElist[i]+1)*increase for i in range(len(OElist))]
+    expectOE = 0
+    trueOE = 0
+    for i in range(len(OElist)):
+        if h1Range[i] > expectOpt:
+            expectOE = OElist[i]
+            break
+    for i in range(len(OElist)):
+        if h1Range[i] > trueOpt:
+            trueOE = OElist[i]
+            break
+    rbPrecent = abs(trueOpt-expectOpt)/trueOpt
+    rbOE = abs(trueOE-expectOE)/trueOE
+    return rbPrecent, rbOE
 # get range of sqrt beta
+def getH1Range(a):
+    #
+    point = 1/(2*a)
+    ll = min([(1-abs(2*a-1))/(2*a),(1+abs(2*a-1))/(2*a)])
+    uu = max([(1-abs(2*a-1))/(2*a),(1+abs(2*a-1))/(2*a)])
+    optH1 = int(h * point) 
+    rangeH1 = (int(h * ll),int(h * uu))
+    return optH1, rangeH1
 
+def measureH1Range(OElist):
+    # got from OE list
+    baseline = OElist[9]
+    optIdx = -1
+    minOE = -1
+    idxList = []
+    for i in range(len(OElist)):
+        if OElist[i]<baseline:
+            idxList.append(i)
+        if not minOE == -1 and minOE > OElist[i]:
+            minOE = OElist
+            optIdx = i 
+        else:
+            minOE = OElist
+            optIdx = i  
+    
+    ll = (min(idxList)+1) * increase
+    uu = (max(idxList)+1) * increase
+    opt = (optIdx+1) * increase
+    return (int(ll), int(uu)), int(opt)
 
-AlphaList = []
+def getMedium(valueList):
+    valueList.sort()
+    return (valueList[int(len(valueList)/2)] + valueList[~int(len(valueList)/2)])/ 2
+
+evaluateRad = []
+evaluateTop = []
+
 for ds in dataset:
-    meanList = []
-    mediumList = []
-
-    for repeat in range(repeatNumber):
+    resultDict = {'w':w,'h':h,'ds':ds[2]}
+    AlphaDict = []
+    for i in range(len(percent)):
+        meanList = []
+        mediumList = []
+        #for repeat in range(repeatNumber):
         dictKeyList = set([])
         nodeDict = {} #{}
-        with open(ds[0],'r') as f:
+        path = homePath+ds[2]+'_'+str(percent[i])+'.txt'
+        with open(path,'r') as f:
             # out degree
             for line in f:
                 line = line.strip()
-                if not len(line) > 0:
-                    continue
-                
-                if s in dictKeyList:
-                    nodeDict[s][1] += freq
-                else:
-                    nodeDict[s] = [0,0]
-                    nodeDict[s][1] += freq
-                    dictKeyList.add(s)
+            if not len(line) > 0:
+                continue
+            parts = line.split(' ')
+            s = int(parts[0]);t = int(parts[1]);freq = float(parts[2])
+            # out degree
+            if s in dictKeyList:
+                nodeDict[s][1] += freq
+            else:
+                nodeDict[s] = [0,0]
+                nodeDict[s][1] += freq
+                dictKeyList.add(s)
             # in degree
             if t in dictKeyList:
                 nodeDict[t][0] += freq
@@ -109,79 +157,68 @@ for ds in dataset:
                 nodeDict[t] = [0,0]
                 nodeDict[t][0] += freq
                 dictKeyList.add(t)
-        outList = []
-        inList = []
-        for key in list(dictKeyList):
-            inList.append(nodeDict[key][0])
-            outList.append(nodeDict[key][1])
 
-        inList.sort(); outList.sort()
+        aList = []
+        with open(ds[0],'r') as f:
+            for line in f:
+                line = line.strip()
+                if not len(line) > 0:
+                    continue
+                s = int(parts[0]);t = int(parts[1]);freq = int(float(parts[2]))
+                # alpha = (i,*)/(*,j)
+                a = nodeDict[s][1]/nodeDict[t][0] # * freq
+                aList.append(a)
+        aList.sort()
+        alphaMAX = max(aList)
+        alphaMEAN = np.mean(aList)
+        alphaMEDIUM = getMedium(aList)
 
-        meanOut = np.mean(outList)
-        mediumOut = (outList[int(len(outList)/2)] + outList[~int(len(outList)/2)])/ 2
-        meanIn = np.mean(inList)
-        mediumIn = (inList[int(len(inList)/2)] + inList[~int(len(inList)/2)])/ 2
+        AlphaDict.append([alphaMEAN,alphaMEDIUM,alphaMAX])
 
-        alphaMEAN = meanOut/meanIn
-        alphaMEDIUM = mediumOut/mediumIn
-        meanList.append(alphaMEAN)
-        mediumList.append(alphaMEDIUM)
+    # check optimal range 
+    datasetRad = diyTool.loadPickle(Q4result_Rad_Path)
+    datasetTop = diyTool.loadPickle(Q4result_Top_Path)
+    # check random 
+    recordDict = getRecord(w,h,ds[2],'rad',datasetRad) #[3][100]
+    evaluateCG =  [[[0 for _ in range(len(recordDict))] for _ in range(len(aList))] for _ in range(len(percent))]
+    evaluateRB = copy.deepcopy(evaluateCG)
+    for i in range(len(percent)): # 
+        aList = AlphaDict[i]  #[3]
+        for j in range(len(aList)): #  3 types of getting alpha mean/medium/max
+            h1h2,opt = getH1Range(aList[j])
+            for k in range(len(recordDict)): # 3 types of evaluating observed error
+                h1h2Data,optData = measureH1Range(recordDict[k])
+                cgV = Coverage(h1h2,h1h2Data,recordDict[k])
+                evaluateCG[i][j][k] = cgV
+                rbV = RelativeBias(opt,optData,recordDict[k])
+                evaluateRB[i][j][k] = rbV
 
+    tem = copy.deepcopy(resultDict)
+    tem['Coverge'] = evaluateCG
+    tem['RelativeBias'] = evaluateRB
+    tem['type'] = 'rad'
+    evaluateRad.append(tem)
 
-    np.mean(meanList)
-    for repeat in range(repeatNumber):
-        [repeat]
+    recordDict = getRecord(w,h,ds[2],'top',datasetTop) #[3][100]
+    evaluateCG =  [[[0 for _ in range(len(recordDict))] for _ in range(len(aList))] for _ in range(len(percent))]
+    evaluateRB = copy.deepcopy(evaluateCG)
+    for i in range(len(percent)): # 
+        aList = AlphaDict[i]  #[3]
+        for j in range(len(aList)): #  3 types of getting alpha mean/medium/max
+            h1h2,opt = getH1Range(aList[j])
+            for k in range(len(recordDict)): # 3 types of evaluating observed error
+                h1h2Data,optData = measureH1Range(recordDict[k])
+                cgV = Coverage(h1h2,h1h2Data,recordDict[k])
+                evaluateCG[i][j][k] = cgV
+                rbV = RelativeBias(opt,optData,recordDict[k])
+                evaluateRB[i][j][k] = rbV
 
-    (ll,uu)
-    #get h1 range
-
-    #取最好的那次
-
-
-#get sample
-
-repeat
-
-
-
-
-
-#
-
-
-
-
-
-
-
-
-# hash function
-
-
-'''
-抽取sample的大小，都抽多一些
-200M
-'''
-
-
-
-
-
-
-
-
-def function():
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
+    tem = copy.deepcopy(resultDict)
+    tem['Coverge'] = evaluateCG
+    tem['RelativeBias'] = evaluateRB
+    tem['type'] = 'top'
+    evaluateTop.append(tem)
+    
+    diyTool.savePickle(Q4evaluate_Rad_Path,evaluateRad)
+    diyTool.savePickle(Q4evaluate_Top_Path,evaluateTop)
 
--- experiment/experimentQ4_1.py
+++ experiment/experimentQ4_1.py
@@ -20,24 +20,22 @@ num2 = 100
 increase = 30
 h = 300
 dataset = [ 
-    ['D:/google desk PC/graph_freq_comp18.txt',338239,2,'comp18', 0.80],
-    ['D:/google desk PC/graph_freq_comp16.txt',1391333,2,'comp16', 0.80],
-    ['D:/google desk PC/graph_freq_comp14.txt',7904564,2,'comp14', 0.60],
-    ['D:/google desk PC/ip_graph_refined',4213084,2,'ip', 0.70],
-    ['D:/google desk PC/tweet_stream_hashed_refined',17813281,2,'tweet', 0.50],  #here
+    #['D:/google desk PC/graph_freq_comp18.txt',338239,'comp18', 0.80],
+    #['D:/google desk PC/graph_freq_comp16.txt',1391333,2,'comp16', 0.80],
+    #['D:/google desk PC/graph_freq_comp14.txt',7904564,2,'comp14', 0.60],
+    #['D:/google desk PC/ip_graph_refined',4213084,2,'ip', 0.70],
+    ['D:/google desk PC/tweet_stream_hashed_refined',17813281,'tweet']#
     #['C:/Users/alfonso.yan/Documents/graph_freq_comp12.txt',338239,2,'comp18', 90],
-    ['D:/google desk PC/graph_freq_comp10.txt',1372146644,2,'comp1', 0.03]
+    #['D:/google desk PC/graph_freq_comp10.txt',1372146644,2,'comp1', 0.03]
 ]
-repeatNumber = 10 #repeat times
 datasetRad = []
 datasetTop = []
 #================ <- parameter
 
 #===================  path area ->
-
 homePath = 'D:/Alfonso Ngan/Documents/Github Project/Sketch-for-Data-Stream/experiment/result/'# use '/' as ending
-Q4result_ResultTop_Path = homePath+'Q4_ResultTop'
-Q4result_ResultRad_Path = homePath+'Q4_ResultRad'
+Q4result_Top_Path = homePath+'Q4_Top'
+Q4result_Rad_Path = homePath+'Q4_Rad'
 #===================  <- path area
 
 def getMedium(valueList):
@@ -51,16 +49,6 @@ def getH1H2(num1,num2,h):
         h2 = int(h**2/h1)
         h1h2List.append((h1,h2))
     return h1h2List
-
-def getValue(twoDlist):
-    # work for all the sketch
-    result = [] # result = [100]
-    for i in range(num2-num1):
-        valueList = []
-        for repeat in range(repeatNumber):
-            valueList.append(twoDlist[repeat][i])
-        result.append(np.mean(valueList))
-    return result
 
 def getRadList(num,radPool):
     radList = [[] for i in range(5)]
@@ -144,23 +132,15 @@ def evaluate_rad_sum(sketch,radList):
 
 for ds in dataset:
     print('========dataset:  '+str(ds))
-    #top = [100,500,1000,2000,5000] rand=[500,1000,2000,5000,10000]
-    oeListTop = [[[] for _  in range(3) ] for _ in range(5)]
-    oeListRad = [[[] for _  in range(3) ] for _ in range(5)]
-    '''
-    oeListTop100Dict=[[] for _ in range(3)];  oeListRad100Dict=[[] for _ in range(3)]
-    oeListTop500Dict=[[] for _ in range(3)];  oeListRad500Dict=[[] for _ in range(3)]
-    oeListTop1000Dict=[[] for _ in range(3)]; oeListRad1000Dict=[[] for _ in range(3)]'''
-    # three types of oe evaluation mean/medium/sum, 100 sketches  oeListTop100Dict = [3][100]
-    #for repeat in range(repeatNumber):
-    #print('============repeat: '+str(repeat))
-    rad1000List = [];top1000List = []
+    topNum = [100,500,1000,2000,5000] 
+    radNum = [500,1000,2000,5000,10000]
+    rad10000List = [];top5000List = []
     countNum = 0
     h1h2List = getH1H2(num1,num2,h)
     sketchList = []
     for i in range(len(h1h2List)):
         h1,h2 = h1h2List[i]
-            #print('for %d, h1 is %d   h2 is %d'%(i,h1,h2))
+        print('for %d, h1 is %d   h2 is %d'%(i,h1,h2))
         hS = copy.deepcopy(hSketch.sketch(w,h1,h2,ds[1]))
         sketchList.append(hS)
     radPool = []
@@ -168,103 +148,83 @@ for ds in dataset:
     with open(ds[0],'r') as f:
         for line in f:
             line = line.strip()
-            if not len(line)>0:
-                    continue
+            if len(line)>0:
                 countNum += 1
                 if countNum % 1000000 == 0:
                     print('now is '+str(countNum))
                 parts = line.split(' ')
                 s = int(parts[0])
                 t = int(parts[1])
-                freq = int(float(parts[2]))
+                freq = float(parts[2])
 
-                if random.randint(0,10000)>10000 * ds[4]:
-                    continue
+                #if random.randint(0,10000)>10000 * 0.5:
+                #    continue
 
                 # get rad and top
-                if random.randint(0,10000)<10000* ds[4] * 0.1:
+                if random.randint(0,10000)<10000 * 0.2:
                     radPool.append([s,t,freq])
                 
-                if len(top1000List)>1000:
-                    minV = min(top1000List, key=lambda x: x[2])
+                if len(top5000List)>5000:
+                    minV = min(top5000List, key=lambda x: x[2])
                     if freq>minV[2]:
-                        indx = top1000List.index(minV);top1000List[indx] = [s,t,freq]
+                        indx = top5000List.index(minV);top5000List[indx] = [s,t,freq]
                 else:
-                    top1000List.append([s,t,freq])
+                    top5000List.append([s,t,freq])
 
                 # update 
                 for i in range(len(sketchList)):
                     sketchList[i].update((s,t),freq)
 
-        print('========evaluation')# evaluation
-        top1000List.sort(reverse = False)
-        top100List = top1000List[:100]
-        top500List = top1000List[:500]
-        rad100List = getRadList(100,radPool)
-        rad500List = getRadList(500,radPool)
-        rad1000List = getRadList(1000,radPool)
+    print('========evaluation')# evaluation
+    topList = []; radList = []
+    top5000List.sort(reverse = False)
+    for i in range(len(topNum)):
+        topList.append(top5000List[:topNum[i]])
+        radList.append(getRadList(radNum[i],radPool))
+    del radPool # clean
 
-        oeListTop100=[[] for _ in range(3)];oeListRad100=[[] for _ in range(3)] # three types of oe evaluation mean/medium/sum
-        oeListTop500=[[] for _ in range(3)];oeListRad500=[[] for _ in range(3)]
-        oeListTop1000=[[] for _ in range(3)];oeListRad1000=[[] for _ in range(3)]
+    sketchOE = {'h':h,'w':w,'ds':ds[2]}
+    tem = copy.deepcopy(sketchOE)
+    # topList
+    print('----------top')# random
+    for j in range(len(topList)): # 5
+        tem[topNum[j]] = {}
+        top_mean = []
+        top_medium = []
+        top_sum = []
+        print('====now is '+str(topNum[j]))
+        for i in range(len(sketchList)): #150?
+            ObservedError = evaluate_top_mean(sketchList[i],topList[j]);top_mean.append(ObservedError)
+            print()
+            ObservedError = evaluate_top_medium(sketchList[i],topList[j]);top_medium.append(ObservedError)
+            print()
+            ObservedError = evaluate_top_sum(sketchList[i],topList[j]);top_sum.append(ObservedError)
+            print()
+        tem[topNum[j]]['top_mean'] = top_mean
+        tem[topNum[j]]['top_medium'] = top_medium
+        tem[topNum[j]]['top_sum'] = top_sum
+    datasetTop.append(tem)
 
-        for i in range(len(sketchList)):
-            print('********for %d'%i)
-            print('----------Top')# top
-            ObservedError = evaluate_top_mean(sketchList[i],top100List);oeListTop100[0].append(ObservedError)
-            ObservedError = evaluate_top_mean(sketchList[i],top500List);oeListTop500[0].append(ObservedError)
-            ObservedError = evaluate_top_mean(sketchList[i],top1000List);oeListTop1000[0].append(ObservedError)
+    # radList
+    print('----------random')# random
+    for j in range(len(radList)): # 5
+        tem[radNum[j]] = {}
+        rad_mean = []
+        rad_medium = []
+        rad_sum = []
+        print('====now is '+str(radNum[j]))
+        for i in range(len(sketchList)): #150?
+            ObservedError = evaluate_rad_mean(sketchList[i],radList[j]);rad_mean.append(ObservedError)
             print()
-            ObservedError = evaluate_top_medium(sketchList[i],top100List);oeListTop100[1].append(ObservedError)
-            ObservedError = evaluate_top_medium(sketchList[i],top500List);oeListTop500[1].append(ObservedError)
-            ObservedError = evaluate_top_medium(sketchList[i],top1000List);oeListTop1000[1].append(ObservedError)
+            ObservedError = evaluate_rad_medium(sketchList[i],radList[j]);rad_medium.append(ObservedError)
             print()
-            ObservedError = evaluate_top_sum(sketchList[i],top100List);oeListTop100[2].append(ObservedError)
-            ObservedError = evaluate_top_sum(sketchList[i],top500List);oeListTop500[2].append(ObservedError)
-            ObservedError = evaluate_top_sum(sketchList[i],top1000List);oeListTop1000[2].append(ObservedError)
+            ObservedError = evaluate_rad_sum(sketchList[i],radList[j]);rad_sum.append(ObservedError)
             print()
-
-            print('----------random')# random
-            ObservedError = evaluate_rad_mean(sketchList[i],rad100List);oeListRad100[0].append(ObservedError)
-            ObservedError = evaluate_rad_mean(sketchList[i],rad500List);oeListRad500[0].append(ObservedError)
-            ObservedError = evaluate_rad_mean(sketchList[i],rad1000List);oeListRad1000[0].append(ObservedError)
-            print()
-            ObservedError = evaluate_rad_medium(sketchList[i],rad100List);oeListRad100[1].append(ObservedError)
-            ObservedError = evaluate_rad_medium(sketchList[i],rad500List);oeListRad500[1].append(ObservedError)
-            ObservedError = evaluate_rad_medium(sketchList[i],rad1000List);oeListRad1000[1].append(ObservedError)
-            print()
-            ObservedError = evaluate_rad_sum(sketchList[i],rad100List);oeListRad100[2].append(ObservedError)
-            ObservedError = evaluate_rad_sum(sketchList[i],rad500List);oeListRad500[2].append(ObservedError)
-            ObservedError = evaluate_rad_sum(sketchList[i],rad1000List);oeListRad1000[2].append(ObservedError)
-            print()
-
-        for i in range(3):
-            oeListTop100Dict[i].append(oeListTop100[i]);oeListRad100Dict[i].append(oeListRad100[i])
-            oeListTop500Dict[i].append(oeListTop500[i]);oeListRad500Dict[i].append(oeListRad500[i])
-            oeListTop1000Dict[i].append(oeListTop1000[i]);oeListRad1000Dict[i].append(oeListRad1000[i])
-
-        # delete
-        del sketchList; del radPool
-    
-    print('========getting oe')# evaluation
-    # getting
-    oeRad100 = [[] for _ in range(3)];oeTop100 = [[] for _ in range(3)]
-    oeRad500 = [[] for _ in range(3)];oeTop500 = [[] for _ in range(3)]
-    oeRad1000 = [[] for _ in range(3)];oeTop1000 = [[] for _ in range(3)]
-    # three type evaluation mean/medium/sum 100 sketch [3][100]
-    for i in range(3):
-        oeRad100[i] = getValue(oeListRad100Dict[i])
-        oeTop100[i] = getValue(oeListTop100Dict[i])
-        oeRad500[i] = getValue(oeListRad500Dict[i])
-        oeTop500[i] = getValue(oeListTop500Dict[i])
-        oeRad1000[i] = getValue(oeListRad1000Dict[i])
-        oeTop1000[i] = getValue(oeListTop1000Dict[i])
-
-    temDict = {'w':w,'h':h,'dataset':ds[3],'type':'Rad','100':oeRad100,'500':oeRad500,'1000':oeRad1000}
-    datasetRad.append(temDict)
-    temDict = {'w':w,'h':h,'dataset':ds[3],'type':'Top','100':oeTop100,'500':oeTop500,'1000':oeTop1000}
-    datasetTop.append(temDict)
+        tem[radNum[j]]['rad_mean'] = rad_mean
+        tem[radNum[j]]['rad_medium'] = rad_medium
+        tem[radNum[j]]['rad_sum'] = rad_sum
+    datasetRad.append(tem)
 
     print('========saving') # saving .......
-    diyTool.savePickle(Q4result_ResultRad_Path,datasetRad)
-    diyTool.savePickle(Q4result_ResultTop_Path,datasetTop)
+    diyTool.savePickle(Q4result_Top_Path,datasetTop)
+    diyTool.savePickle(Q4result_Rad_Path,datasetRad)
