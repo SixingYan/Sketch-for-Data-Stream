e0d8d711f34b74a66a3fde3e0c16f62d4342971b
Yan, Sixing <plutoyem@outlook.com>
2018-01-10 10:15:26 +0800

    new

--- experiment/experimentQ1_1.py
+++ experiment/experimentQ1_1.py
@@ -3,9 +3,9 @@
 import random
 percent = [0.001, 0.003, 0.005, 0.01, 0.03, 0.05, 0.1, 0.2]
 dataset = [ 
-    ['D:/google desk PC/graph_freq_comp18.txt',338239,568532,'comp18']#,
-    #['C:/Users/alfonso.yan/Documents/graph_freq_comp16.txt',1391333,2815263,'comp16'],
-    #['C:/Users/alfonso.yan/Documents/graph_freq_comp14.txt',7904564,20441831,'comp14'],
+    #['D:/google desk PC/graph_freq_comp18.txt',338239,568532,'comp18']#,
+    ['D:/google desk PC/graph_freq_comp16.txt',1391333,2815263,'comp16']#,
+    #['D:/google desk PC/graph_freq_comp14.txt',7904564,20441831,'comp14'],
     #['C:/Users/alfonso.yan/Documents/ip_graph_refined',4213084,12714850,'ip'],
     #['C:/Users/alfonso.yan/Documents/tweet_stream_hashed_refined',17813281,78508963,'tweet'],
     #['C:/Users/alfonso.yan/Documents/graph_freq_comp12.txt',338239,2,'comp18'], ?
@@ -17,7 +17,7 @@ home = 'D:/google desk PC/sample/'
 def getSamplePool(path, samplePool,percent):
     with open(path,'r') as f:
         for line in f:
-            if len(line)>0:
+            if len(line.strip())>0:
                 if not random.randint(0,10000) < 10000 * percent:
                     continue # rej
                 samplePool.append(line)
@@ -32,6 +32,7 @@ for ds in dataset:
         while len(samplePool) < ds[2] * percent[i]:
             print('=====get sampling!')
             getSamplePool(ds[0], samplePool, percent[i])
+            
         print('======second sampling!')
         if len(samplePool) > ds[2] * percent[i]:
             for _ in range(int(ds[2] * percent[i])):
