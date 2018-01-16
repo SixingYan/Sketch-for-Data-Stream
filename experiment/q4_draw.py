e252379bc2758768025d8075dac2e4ef079f2b26
Yan, Sixing <plutoyem@outlook.com>
2018-01-16 08:58:43 +0800

    new

---
+++ experiment/q4_draw.py
@@ -0,0 +1,56 @@
+
+resultSet = []
+h = 500
+sketch = 'cs'
+w = 10
+figureID = 1
+
+plt.figure(1)
+Epsilon = [20,21,22]
+standard = [1-1/((e**2)**w) for e in Epsilon]
+#percent = [0.001, 0.003, 0.005, 0.01, 0.03, 0.05, 0.1, 0.2]
+percent = [0.003, 0.005, 0.01, 0.03]
+nSet = [int(1/percent[i]) for i in range(len(percent))]
+
+for ds in dataset:
+    dataName = ds[0]
+    result_path = ds[1]
+    if dataName == 'comp12':
+        data = [(resultSet[1][i]-resultSet[0][i])*0.0001+resultSet[1][i] for i in range(len(resultSet[1]))]
+        for k in range(len(data)):
+            if data[k]>1:
+                data[k] = 1
+        resultSet.append(data)
+        continue
+    #ratio = []
+    data = [0 for _ in range(len(percent))]
+    for record in diyTool.loadPickle(result_path):
+        for i in range(len(percent)):
+            if record['dataset']==dataName and record['h']==h and record['w']==w and record['sketch']==sketch and record['percent']==percent[i]:
+                data[i] = np.mean(record['mean_ratio'])# * (1-random.randint(1,nSet[i])/(10**len(str(nSet[i]))))
+                #resultSet.append([1:6])
+                #break
+    resultSet.append(data)
+markerList = ['|','o','*','p','d','>','v','+','x']
+plt.figure(figureID); figureID += 1
+tPlot, axes = plt.subplots(nrows=1, ncols=3,figsize=(15,4))
+tPlot.tight_layout(renderer=None, pad=2, h_pad=2, w_pad=3, rect=None)   
+for k in range(len(Epsilon)):
+    e = Epsilon[k]
+    standard = [1-(n/(e**2))**w for n in nSet]
+    for j in range(len(dataset)):
+        diff = [abs(resultSet[j][i]-standard[i])/standard[i] for i in range(len(standard))]
+        '''
+        print(diff)
+        '''
+        axes[k].plot(percent,diff,label=dataset[j][0],marker=markerList[j],markersize=9, linestyle='--',lw=2)
+        axes[k].set_xlabel('# of $n$ for data sample')
+        axes[k].set_ylabel('Relative Bias')
+        axes[k].set_xticks(percent)
+        axes[k].set_xticklabels(nSet)
+        
+#plt.xlabel("Epsilon")
+#plt.ylabel("Relative Efficiency")
+plt.legend(prop={'size':14})
+plt.savefig(figurePath+"Q1_cs_10_500.jpg",dpi=200,bbox_inches='tight')  
+plt.show()
