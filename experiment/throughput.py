761b02a745fd48ff196d2006b2ff03109e123a88
Yan, Sixing <plutoyem@outlook.com>
2018-01-26 09:30:20 +0800

    new

---
+++ experiment/throughput.py
@@ -0,0 +1,38 @@
+'''
+run on bigmachine
+'''
+
+
+
+
+
+
+import time
+import mSketch
+#import lib.mSketch as mSketch
+
+
+
+tC = 0
+tG = 0
+tM = 0
+h = 20
+'[(),(),(),()]',
+
+hListM = [[512*10**8,128*10**8],[],[10,400,800,200,40]]
+
+straM = [(0),(1)],[(0),(1,2),(3)],[(1),(2,3),(4,7),(0,5),(6)]
+straC = [(0,1)],[(0,1,2,3)],[(0,1,2,3,4,5,6,7)],
+
+for i in range(len(partNum)):
+    [tuple(i) for i in range(partNum[i])]
+
+
+
+
+
+
+
+
+
+
