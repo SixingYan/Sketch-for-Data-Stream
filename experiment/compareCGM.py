3522cff67488726b80e3999d42cfa327e05affc1
Yan, Sixing <plutoyem@outlook.com>
2018-01-28 13:11:31 +0800

    new

--- experiment/compareCGM.py
+++ experiment/compareCGM.py
@@ -1,6 +1,53 @@
+'''
+
+'''
+#----------------
+def getPathDict(pathStr):
+    # input a string 
+    pathDict = {}
+    pathDict['partID'] = []
+    pathDict['edgeType'] = []
+    preIndex = 0
+    for i in range(len(pathStr)):
+        if pathStr[i] == 'S' or pathStr[i] == 'C':
+            pathDict['partID'].append(int(pathStr[preIndex:i]))
+            if pathStr[i] == 'S':
+                pathDict['edgeType'].append(0)
+            else:
+                pathDict['edgeType'].append(1)
+            preIndex = i + 1
+    pathTem = pathStr[::-1]# reverse string
+    idx = 0
+    for i in range(len(pathTem)):
+        if pathTem[i] == 'S' or pathTem[i] == 'C':
+            idx = i
+            break
+    pathDict['partID'].append(int(pathStr[-idx:]))
+    return pathDict   
+#----------------
+def getStrategy(pathDict):
+    # edge=0 seperate  edge=1 combine 
+    j = 0
+    strategy = []#[[],...[]]
+    for i in range(len(pathDict['partID'])):
+        if i == len(pathDict['partID'])-1:
+            continue
+        if i == 0:
+            strategy.append([])
+            strategy[j].append(pathDict['partID'][i])
+        edge = pathDict['edgeType'][i]
+        if edge == 1:
+            strategy[j].append(pathDict['partID'][i+1])
+        else:
+            strategy.append([])
+            j += 1
+            strategy[j].append(pathDict['partID'][i+1])
+    return strategy
+
+dataset = ['tr_1', '/data1/Sixing/tr_1_4ij', '/data1/Sixing/tr_1_2', '/data1/Sixing/tr_1']
 
 
-
+hSet = [1000,100,10]
 stra = [[0, 1]]
 hList =  [400]
 maxIDList = [255255]
@@ -20,6 +67,7 @@ w = 10
 strList = []
 
 for i in range(len(partNum)):
+    sketchList = []
     if not i == 1: # only for 4-parts
         continue
     hListG = [hSet[i] for _ in range(partNum[i])]
@@ -29,7 +77,55 @@ for i in range(len(partNum)):
     mG = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListG,w,hSet[i],straG,partNum[i]));mG.buildSketch()
     
     for path in strList:
-        
+        d = getPathDict(path)
+        stra = getStrategy(d)
+        mS = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListM[i],w,hSet[i],stra,partNum[i]));mS.buildSketch()
+        sketchList.append(mS)
+
+    with open(dataset[i+1],'r') as f:
+        # input structure of sketch 
+        # open a sample of stream partList, e.g., 5,6,7
+        pool = []
+        print('getting stream ==========> ')
+        for line in f:
+            line = line.strip()
+            if not len(line) > 0:
+                continue
+            countNum += 1
+            if countNum > 1000000:
+                break
+            parts = line.split(' ')
+            #print('line '+line)
+            # should be multi-part
+            if partNum[i]> 5: # for 8 parts
+                try:
+                    sNode = [int(k) for k in parts[0].split('.')];
+                    tNode = [int(k) for k in parts[1].split('.')];
+                except:
+                    continue
+                fre = float(parts[2])
+                nodeList = sNode + tNode
+                #print('8 parts')
+            elif partNum[i]> 3 :# for 4 parts
+                nodeList = [int(k) for k in parts[:4]]
+                #print('4 parts')
+            else:
+                nodeList = [int(k) for k in parts[:2]]
+
+            fre = float(parts[-1])
+            edge = []
+            #print('nodeList '+str(nodeList))
+            for pID in partList[i]:
+                edge.append(nodeList[pID])
+            #print(edge)
+            #print(fre)
+
+            for num in sketchList:
+                sketchList[num].update(edge,fre)
+
+            mC.update(edge,fre)
+
+            mG.update(edge,fre)
 
 
 
