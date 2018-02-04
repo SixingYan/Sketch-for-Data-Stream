3485ab44892ac61fa951814c8c1515be65d424bb
Yan, Sixing <plutoyem@outlook.com>
2018-02-04 14:07:00 +0800

    new

--- experiment/compareCGM.py
+++ experiment/compareCGM.py
@@ -1,25 +1,27 @@
-'''
 
-'''
 import copy
-import time
+#import time
 import mSketch2D
-#import lib.mSketch as mSketch
-import numpy as np 
+#import numpy as np 
+import random
 maxIDList = [
-[255255255255 for _ in range(2)],
+#[255255255255 for _ in range(2)],
 [255255 for _ in range(4)],
 [255 for _ in range(8)],
 ]
-strList = [
-[],
-[],
-['7S6C5S4C2C1C0S3','7C5S6C4C3S2S1C0','7C6C5S4S3C2C0S1','7S6C5C3C1S4S2S0','7C5C4S6C2S3S1C0','7S6S5S4C2S3C0S1',
- '7C6S5S4C3S2C0S1','7C6C0S5S4S3C2S1','7C5S6C3S4C1S2S0','7C4C3S6C5C2C0S1','7S6C3C1S5C0S4S2','7C6S5C3C1S4C2C0',
- '7C1S6C5S4S3S2C0','7C6S5S4C2C0S3C1','7C0S6C3C2C1S5S4','7C4C3C2S6C5C1S0','7S6C1S5C3C2C0S4','7C3C0S6C2S5C4C1','7C5C1S6S4S3S2C0',
- '7S6C5C4C1S3C0S2','7C5S6S4C3C2C0S1','7S6C3S5C2C0S4S1','7C5C3S6C0S4C2S1','7C5C4S6S3C2C1C0','7S6C3S5C1S4S2S0',
- '7S6C2S5S4C3C0S1','7C6S5C2S4S3S1C0','7C5C2C1S6C0S4S3','7C5S6C2C0S4S3C1','7C6C4S5C0S3S2S1','7S6C4S5C1C0S3S2'],
-]
+#print('yes')
+#strList = ['7S6C5S4C2C1C0S3','7C5S6C4C3S2S1C0','7C6C5S4S3C2C0S1','7S6C5C3C1S4S2S0','7C5C4S6C2S3S1C0','7S6S5S4C2S3C0S1',
+# '7S6C2S5S4C3C0S1','7C6S5C2S4S3S1C0','7C5C2C1S6C0S4S3','7C5S6C2C0S4S3C1','7C6C4S5C0S3S2S1','7S6C4S5C1C0S3S2']
+#strList = ['7C6C5C4C3C2C1C0','7S6S5S4S3S2S1S0']
+
+def getRadList(num,radPool):
+    radList = [[] for i in range(5)]
+    for i in range(len(radList)):
+        while len(radList[i]) < num:
+            tem = random.choice(radPool)
+            if tem not in radList[i]:
+                radList[i].append(tem)
+    return radList
 def evaluate_rad_sum(sketch,radList):
     #
     ObservedError = 0
@@ -85,7 +87,7 @@ def getStrategy(pathDict):
             strategy[j].append(pathDict['partID'][i+1])
     return strategy
 #-------------
-
+'''
 def getHList(stra):
     #
     hList = []
@@ -93,11 +95,10 @@ def getHList(stra):
         l = h**len(stra[i])
         hList.append(l)
     return hList
-
-dataset = ['tr_1', '/data1/Sixing/tr_1_4ij', '/data1/Sixing/tr_1_2', '/data1/Sixing/tr_1']
+'''
+dataset = ['tr_1', '/data1/Sixing/tr_1_2', '/data1/Sixing/tr_1']
 
 '''
-
 stra = [[0, 1]]
 hList =  [400]
 maxIDList = [255255]
@@ -105,48 +106,47 @@ w = 10
 h = 20
 edge = [60764, 32817]
 '''
-hSet = [1000,100,10]
-h = 10
-partNum = [2,4,8]
+hSet = [31.7,5.7]
+#h = 10
+partNum = [4,8]
 #hListM = [[],[],[4,100,50,200,25]]
-hListC = [[10**6,],[10**8,],[10**8,]]
+hListC = [10**6]
 #straM = [[0,],[1,]],[(0,),(1,2),(3,)],[(1,),(2,3),(4,7),(0,5),(6,)]
-straC = [[0,1,]],[(0,1,2,3)],[(0,1,2,3,4,5,6,7)],
-partList = [[0,1],[0,1,2,3],[0,1,2,3,4,5,6,7]]
-w = 10
-
-strList = []
-
+straC = [[(0,1,2,3)],[(0,1,2,3,4,5,6,7)]]
+partList = [[0,1,2,3],[0,1,2,3,4,5,6,7]]
+w = 4
+countNum = 0
+#strList = []
+radPool = []
+top100List = []
 for i in range(len(partNum)):
-    sketchList = []
-    if not i == 2: # only for 8-parts
-        continue
-    hListG = [hSet[i] for _ in range(partNum[i])]
+    #sketchList = []
+    hListG = [pow(1000000,1/partNum[i]) for pn in range(partNum[i])]
     straG = [[j,] for j in range(partNum[i])]
     #mS = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListM[i],w,hSet[i],straM[i],partNum[i]));mS.buildSketch()
-    mC = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListC[i],w,hSet[i],straC[i],partNum[i]));mC.buildSketch()
+    mC = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListC,w,hSet[i],straC[i],partNum[i]));mC.buildSketch()
     mG = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListG,w,hSet[i],straG,partNum[i]));mG.buildSketch()
-    
-    for path in strList[i]:
+    '''
+    for path in strList:
         d = getPathDict(path)
         stra = getStrategy(d)
         hListM = getHList(stra)
         mS = copy.deepcopy(mSketch2D.mSketch2D(maxIDList[i],hListM,w,hSet[i],stra,partNum[i]));mS.buildSketch()
         sketchList.append(mS)
-
+    '''
     with open(dataset[i+1],'r') as f:
         # input structure of sketch 
         # open a sample of stream partList, e.g., 5,6,7
         pool = []
         print('getting stream ==========> ')
-        for line in f:
+        for line in f.readlines():
             line = line.strip()
             if not len(line) > 0:
                 continue
             if random.random()>0.3:
                 continue
             countNum += 1
-            if countNum > 2000:
+            if countNum > 1000000:
                 break
             parts = line.split(' ')
             #print('line '+line)
@@ -169,45 +169,46 @@ for i in range(len(partNum)):
             fre = float(parts[-1])
             edge = []
 
-            if len(top5000List)>2000:
-                minV = min(top5000List, key=lambda x: x[2])
-                if freq>minV[2]:
-                    indx = top5000List.index(minV);top5000List[indx] = [edge,freq]
+            if len(top100List)>100:
+                minV = min(top100List, key=lambda x: x[1])
+                if fre>minV[1]:
+                    indx = top100List.index(minV);top100List[indx] = [edge,fre]
             else:
-                top5000List.append([edge,freq])
+                top100List.append([edge,fre])
 
             if random.random()<0.2:
-                radPool.append([edge,freq])
+                radPool.append([edge,fre])
 
             for pID in partList[i]:
                 edge.append(nodeList[pID])
 
-            for num in sketchList:
-                sketchList[num].update(edge,fre)
+            #for num in sketchList:
+            #    sketchList[num].update(edge,fre)
 
             mC.update(edge,fre)
-
             mG.update(edge,fre)
 
     print('========evaluation')# evaluation
-    topList = []; radList = []
-    top5000List.sort(key= lambda d : d[2], reverse = True)
-    topList = top5000List[:1000]
-    radList = getRadList(1000,radPool)
+    #topList = []; radList = []
+    #top100List.sort(key= lambda d : d[1], reverse = True)
+    #topList = top100List[:1000]
+    rad1000List = getRadList(1000,radPool)
     del radPool
-
-    with open('/data1/Sixing/expdata/txt_'+dataset[0]+'_'+str(partNum[i]),'a') as f:
+    print('analysis')
+    with open('/data1/Sixing/expdata/txt__'+dataset[0]+'_'+str(partNum[i]),'a') as f:
+        '''
+        print('MOD')
         f.write('\n MG')
         valueList = []
         for num in range(len(sketchList)):
-            value = evaluate_top_sum(sketchList[i],topList)
+            value = evaluate_top_sum(sketchList[i],top100List)
             valueList.append(value)
         mV = min(valueList)
         idx = valueList.index(mV)
         f.write('top stra '+str(sketchList[idx].sg))
         f.write('top: '+str(mV))
         f.write('\n')
-
+        
         valueList = []
         for num in range(len(sketchList)):
             value = evaluate_rad_sum(sketchList[i],radList)
@@ -217,17 +218,20 @@ for i in range(len(partNum)):
         f.write('rad stra '+str(sketchList[idx].sg))
         f.write('rad: '+str(mV))
         f.write('\n')
+        '''
 
+        print('cSketch')
         f.write('\n MC')
-        value = evaluate_top_sum(mC,topList)
-        f.write('top: '+str(mV))
-        value = evaluate_rad_sum(mC,radList)
-        f.write('rad: '+str(mV))
+        value = evaluate_top_sum(mC,top100List)
+        f.write('top: '+str(value))
+        value = evaluate_rad_sum(mC,rad1000List)
+        f.write('rad: '+str(value))
         f.write('\n')
 
+        print('gMatrix')
         f.write('\n MG')
-        value = evaluate_top_sum(mG,topList)
-        f.write('top: '+str(mV))
-        value = evaluate_rad_sum(mG,radList)
-        f.write('rad: '+str(mV))
+        value = evaluate_top_sum(mG,top100List)
+        f.write('top: '+str(value))
+        value = evaluate_rad_sum(mG,rad1000List)
+        f.write('rad: '+str(value))
         f.write('\n')
